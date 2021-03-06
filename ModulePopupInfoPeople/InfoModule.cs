9db7fcd430fbbf58a4da64683c97cca3a3ea3f16
victor.herrera <victor.herrera@copernicus.es>
2021-03-06 12:11:19 +0100

    First Commit

---
+++ ModulePopupInfoPeople/InfoModule.cs
@@ -0,0 +1,41 @@
+﻿using API;
+using API.Data;
+using API.Services;
+using ModulePopupInfoPeople.ViewModels;
+using ModulePopupInfoPeople.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Mvvm;
+using Prism.Regions;
+using DataProvider;
+
+namespace ModulePopupInfoPeople
+{
+    public class InfoModule : IModule
+    {
+
+        private readonly IRegionManager _regionManager;
+
+        public InfoModule(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
+
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            _regionManager.RegisterViewWithRegion("ContentRegion", typeof(PeopleControl));
+            var infoService = containerProvider.Resolve<IInformationService>();
+            var people = containerProvider.Resolve<IPeopleProvider>();
+            infoService.Register(people);
+
+            
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            ViewModelLocationProvider.Register<PeopleControl, PeopleControlViewModel>();
+            containerRegistry.RegisterSingleton<IInformationService, InformationService>();
+            containerRegistry.RegisterSingleton<IPeopleProvider, PeopleProvider>();
+        }
+    }
+}
---
+++ ModulePopupInfoPeople/ViewModels/PeopleControlViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Windows.Documents;
+using API.Data;
+using API.Services;
+using DataProvider;
+using Model;
+using Prism.Commands;
+using Prism.Ioc;
+using Prism.Mvvm;
+
+namespace ModulePopupInfoPeople.ViewModels
+{
+    public class PeopleControlViewModel: BindableBase
+    {
+        private IInformationService _service;
+        private IContainerProvider _containerProvider;
+        private string _nombreProvider;
+        public PeopleControlViewModel(IInformationService service, IContainerProvider containerProvider)
+        {
+            _service = service;
+
+            _containerProvider = containerProvider;
+            _nombreProvider = _containerProvider.Resolve<IPeopleProvider>().ProviderName;
+        }
+
+        private string _text = "Prueba Command Binding";
+        public string Texto
+        {
+            get => _text;
+            set => SetProperty(ref _text, value);
+        }
+
+        public DelegateCommand GetDataCommand
+        {
+            get
+            {
+                return new DelegateCommand(SetPeople);            
+            }
+        }
+
+        private List<Person> _people;
+
+        public List<Person> People { get { return _people; } set {
+                SetProperty(ref _people, value);
+            } }
+
+        public void SetPeople()
+        {
+            People = _service.GetProvider(_nombreProvider).GetPeople();
+          
+        }
+
+
+    }
+}
